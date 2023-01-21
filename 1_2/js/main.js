f537efbef2f0d727b5197a3855e029744c82caff
narcissus313 <srz.shl@gmail.com>
2023-01-21 14:48:41 +0330

    1_2 ended!

--- 1_2/js/main.js
+++ 1_2/js/main.js
@@ -1,5 +1,7 @@
 let userProfileModal = document.getElementById("userProfileModal");
+let newUserModal = $("#newUserModal");
 let inputSearchbar = document.getElementById("inputSearchbar");
+let newUserModalContent = $("#newUserModal .modal-content");
 let modalHeader = document.querySelector(".modal-header");
 let usersListRow = document.getElementById("usersList");
 let btnSearch = document.getElementById("btnSearch");
@@ -15,6 +17,13 @@ let numberOfCardsPerPage = 6;
 let currentPage = 1;
 let usersData = fetchUserData();
 let lastUsers = usersData;
+let newId = calculateNewId();
+
+function calculateNewId() {
+	let ids = usersData.map((user) => user.id);
+	let max = Math.max.apply({}, ids);
+	return max + 1;
+}
 
 function fetchUserData() {
 	let users = [];
@@ -24,7 +33,6 @@ function fetchUserData() {
 			type: "GET",
 			data: {
 				name: "reza",
-				age: 34,
 			},
 			success: (response) => {
 				users.push(...response.data);
@@ -70,6 +78,63 @@ function fieldRegexChecker(user = null) {
 		}
 		return true;
 	}
+	if (
+		!nameRegex.test($("#creationModal-fnameInput").val().trim()) ||
+		!nameRegex.test($("#creationModal-lnameInput").val().trim())
+	) {
+		alert("invalid first name or last name. just letters and space");
+		return false;
+	} else if (!validateEmail($("#creationModal-emailInput").val())) {
+		alert("Email exists. choose another one");
+		return false;
+	} else if (!emailRegex.test($("#creationModal-emailInput").val().trim())) {
+		alert("Email is wrong");
+		return false;
+	}
+	return true;
+}
+
+function newUserCreator() {
+	newUserModalContent.html(`
+		<div class="modal-header">
+            <h5 class="modal-title text-primary mt-2">New User</h5>
+        </div>
+		<div class="modal-body p-0">
+			<div class="row p-3">
+				<div class="col p-3">
+					<div class="mb-3">
+						<label for="creationModal-fnameInput" class="form-label">Firstname *</label>
+						<input type="text" class="form-control" id="creationModal-fnameInput" value=""/>
+					</div>
+					<div class="mb-3">
+						<label for="creationModal-lnameInput" class="form-label">Lastname *</label>
+						<input type="text" class="form-control" id="creationModal-lnameInput" value=""/>
+					</div>
+					<div class="mb-3">
+						<label for="creationModal-emailInput" class="form-label">Email *</label>
+						<input type="email" class="form-control" id="creationModal-emailInput" value=""/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-success px-4" id="btnCreateConfirm">Create</button>
+		</div>`);
+	btnCreateConfirm = $("#btnCreateConfirm");
+	btnCreateConfirm.on("click", function () {
+		let newUser = {};
+		newUser.id = newId;
+		if (fieldRegexChecker()) {
+			newUser.first_name = $("#creationModal-fnameInput").val();
+			newUser.last_name = $("#creationModal-lnameInput").val();
+			newUser.email = $("#creationModal-emailInput").val();
+			newUser.avatar = "";
+			usersData.push(newUser);
+			newId++;
+			renderUsersList(usersData);
+			newUserModal.modal("hide");
+		}
+	});
 }
 
 function userProfileModalCreator(data) {
@@ -106,7 +171,7 @@ function userProfileModalCreator(data) {
 		btnUpdate.removeClass("d-none");
 		btnDelete.removeClass("d-none");
 		btnEdit.addClass("d-none");
-		console.log(modalBody);
+		// console.log(modalBody);
 		modalBody.html(`
 		<div class="modal-body">
 			<!-- firstname input -->
@@ -144,39 +209,6 @@ function userProfileModalCreator(data) {
 			renderUsersList(usersData);
 			modal.modal("hide");
 		}
-	});
-}
-
-function newUserCreator() {
-	modalContent.html(`
-		<div class="modal-header">
-            <h5 class="modal-title text-primary mt-2">New User</h5>
-        </div>
-		<div class="modal-body p-0">
-			<div class="row p-3">
-				<div class="col p-3">
-					<div class="mb-3">
-						<label for="creationModal-fnameInput" class="form-label">Firstname *</label>
-						<input type="text" class="form-control" id="creationModal-fnameInput" value=""/>
-					</div>
-					<div class="mb-3">
-						<label for="creationModal-lnameInput" class="form-label">Lastname *</label>
-						<input type="text" class="form-control" id="creationModal-lnameInput" value=""/>
-					</div>
-					<div class="mb-3">
-						<label for="creationModal-emailInput" class="form-label">Email *</label>
-						<input type="email" class="form-control" id="creationModal-emailInput" value=""/>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="modal-footer">
-			<button type="button" class="btn btn-success px-4" id="btnEdit">Create</button>
-		</div>`);
-	let modalBody = $(".modal-body");
-	btnCreate = $("#btnCreate");
-	btnCreate.on("click", () => {
-		console.log(modalBody);
 	});
 }
 
@@ -242,7 +274,6 @@ function searchInUsers(value) {
 	}
 	let lastUsers = filteredUsers;
 	renderUsersList(lastUsers);
-	inputSearchbar.value = "";
 }
 
 btnSearch.addEventListener("click", () => searchInUsers(inputSearchBarValue));
